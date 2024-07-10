ed64048ab9774922c6335043bfbff5bd0c3a9ce2
203407 <203407@ids.upchiapas.edu.mx>
2024-07-10 13:31:07 -0600

    feat:Note model

--- Notes/models.py
+++ Notes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Note(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=80)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
